97d1de5e1ff35f9c06dcdc4cd4dd78b41f67cc33
elmo15 <132373694+elmo15@users.noreply.github.com>
2025-06-30 21:00:10 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿#region question 1
+
+//int x = Convert.ToInt32(Console.ReadLine());
+//if (x % 3 == 0 && x % 4 == 0)
+//{
+//    Console.WriteLine("Yes");
+//}
+//else
+//{
+//    Console.WriteLine("No");
+//}
+#endregion
+
+#region question 2
+
+//int x = Convert.ToInt32(Console.ReadLine());
+
+//if (x < 0)
+//{
+//    Console.WriteLine("Negative");
+//}
+//else
+//{
+//    Console.WriteLine("Positive");
+//}
+
+
+#endregion
+
+#region question 3
+
+//int x = Convert.ToInt32(Console.ReadLine());
+//int y = Convert.ToInt32(Console.ReadLine());
+//int z = Convert.ToInt32(Console.ReadLine());
+//int max = Math.Max(x, Math.Max(y, z));
+//int min = Math.Min(x, Math.Min(y, z));
+
+//Console.WriteLine($"Maximum: {max}, Minimum: {min}");
+
+#endregion
+
+#region question 4
+
+//int x = Convert.ToInt32(Console.ReadLine());
+//if (x % 2 == 0)
+//{
+//    Console.WriteLine("Even");
+//}
+//else
+//{
+//    Console.WriteLine("Odd");
+//}
+
+#endregion
+
+#region question 5
+
+//char ch = char.ToLower(Console.ReadKey().KeyChar);
+//Console.WriteLine();
+
+//if ("aeiou".Contains(ch))
+//    Console.WriteLine("Vowel");
+//else
+//    Console.WriteLine("Consonant");
+
+#endregion
