3bfcbf7a2dc12e8500c85f142fd5d9b9e3a894f9
moham <moham@ELMO>
2025-06-30 19:40:55 +0300

    Add project files.

---
+++ ass2/Program.cs
@@ -0,0 +1,76 @@
+﻿#region question 1  
+//int x = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("The number is " + x);
+#endregion
+
+#region question 2  
+//string numberString = "abc";
+//int number = int.Parse(numberString);
+//will not work
+#endregion
+
+#region question 3
+//float x = 10.3f;
+//float y = 20.5f;
+//float z = x + y;
+//Console.WriteLine(z);
+#endregion
+
+#region question 4
+//string a = "hello";
+//string b = a.Substring(1, 4);
+//Console.WriteLine(b);
+#endregion
+
+#region question 5
+//int x=10;
+//int y = x;
+//Console.WriteLine("First y "+ y);
+//y=20;
+//Console.WriteLine("Second y "+ y);
+#endregion
+
+#region question 6
+
+
+
+#endregion
+
+#region question 7
+
+//string a= "hello";
+//string b = "world";
+//string c = a + " " + b;
+//Console.WriteLine(c);
+
+#endregion
+
+#region question 8
+
+//int d;
+//d = Convert.ToInt32(!(30 < 20));
+//Console.WriteLine(d); // 1
+
+#endregion
+
+#region question 9
+
+//Console.WriteLine(13 / 2 + " " + 13 % 2); //d
+
+
+#endregion
+
+
+#region question 10
+
+//int num = 1, z = 5;
+
+
+//if (!(num <= 0))
+//    Console.WriteLine(++num + z++ + " " + ++z);
+//else
+//    Console.WriteLine(--num + z-- + " " + --z);
+
+
+#endregion
